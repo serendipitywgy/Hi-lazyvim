ac43a82110e23f5a85e6f55a46cb3fb73095fb4b
wangguangyu <wangguangyu@maker-ray.com>
2024-11-26 15:30:19 +0800

    [update] 更新默认主题

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "tokyonight.nvim",
-    -- 下面是透明背景，暂时注释
-    -- opts = {
-    --   transparent = true,
-    --   styles = {
-    --     sidebars = "transparent",
-    --     floats = "transparent",
-    --   },
-    -- },
-  },
+  -- {
+  --   "tokyonight.nvim",
+  --   -- 下面是透明背景，暂时注释
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
 
   {
     "ellisonleao/gruvbox.nvim",
