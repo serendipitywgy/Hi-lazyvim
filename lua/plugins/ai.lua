4bb065da9b65e11df8bfa6f22d50c58cbcb609fa
wgy <yuserendipity@foxmail.com>
2024-12-16 00:19:38 +0800

    增加ai插件

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "Kurama622/llm.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    cmd = { "LLMSesionToggle", "LLMSelectedTextHandler" },
+    config = function()
+      require("llm").setup({
+        max_tokens = 512,
+        url = "https://open.bigmodel.cn/api/paas/v4/chat/completions",
+        model = "glm-4-flash",
+        prefix = {
+          user = { text = "😃 ", hl = "Title" },
+          assistant = { text = "⚡ ", hl = "Added" },
+        },
+
+        save_session = false,
+        max_history = 15,
+
+        -- stylua: ignore
+        keys = {
+          -- The keyboard mapping for the input window.
+          ["Input:Submit"]      = { mode = "n", key = "<cr>" },
+          ["Input:Cancel"]      = { mode = "n", key = "<C-c>" },
+          ["Input:Resend"]      = { mode = "n", key = "<C-r>" },
+
+          -- only works when "save_session = true"
+          ["Input:HistoryNext"] = { mode = "n", key = "<C-j>" },
+          ["Input:HistoryPrev"] = { mode = "n", key = "<C-k>" },
+
+          -- The keyboard mapping for the output window in "split" style.
+          ["Output:Ask"]        = { mode = "n", key = "i" },
+          ["Output:Cancel"]     = { mode = "n", key = "<C-c>" },
+          ["Output:Resend"]     = { mode = "n", key = "<C-r>" },
+
+          -- The keyboard mapping for the output and input windows in "float" style.
+          ["Session:Toggle"]    = { mode = "n", key = "<leader>ac" },
+          ["Session:Close"]     = { mode = "n", key = "<esc>" },
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
+      { "<leader>ae", mode = "v", "<cmd>LLMSelectedTextHandler 请解释下面这段代码<cr>" },
+      { "<leader>t", mode = "x", "<cmd>LLMSelectedTextHandler 英译汉<cr>" },
+    },
+  },
+}
