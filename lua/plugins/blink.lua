18933264f02859079dea1c6ce5c4ea5a71bd1706
serendipitywgy <serendipitywgy@gmail.com>
2025-01-09 23:43:50 +0800

    [config] 更新blink插件配置，避免报错

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,11 +1,21 @@
+-- return {
+--   "saghen/blink.cmp",
+--   opts = function(_, opts)
+--     opts.completion = { list = { selection = "auto_insert" } }
+--     opts.keymap = {
+--       preset = "enter",
+--       ["<Tab>"] = { "select_next", "fallback" },
+--       ["<S-Tab>"] = { "select_prev", "fallback" },
+--     }
+--   end,
+-- }
 return {
-  "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.completion = { list = { selection = "auto_insert" } }
-    opts.keymap = {
-      preset = "enter",
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-    }
-  end,
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+      },
+    },
+  },
 }
