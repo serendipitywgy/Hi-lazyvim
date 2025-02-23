c5dd7d2e6ab1f02501eb6dc53dfeb7a4b57fab61
wangguangyu <wangguangyu@maker-ray.com>
2025-02-23 23:15:49 +0800

    [config] update blink-cmp.lua

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        -- 设置 Tab 键向下切换候选词
+        ["<Tab>"] = { "select_next", "fallback" },
+
+        -- 设置 Shift-Tab 键往上切换候选词
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+    },
+  },
+}
